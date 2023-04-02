cb7f4ed3f9ccd2cd17a8625000ea819ae5a098c1
Benjamin Bolgrin <>
2023-04-02 11:09:09 +0200

    Implemented method getMostRequestsBySerial in class SepoLogAnalyzer

--- SepoLogAnalyzer.php
+++ SepoLogAnalyzer.php
@@ -9,21 +9,56 @@ class SepoLogAnalyzer{
 
 	function __construct(SepoLog $sepoLog){
 
-		$this->sepoLog = $sepoLog;
+		$this->sepoLog = $sepoLog->getLogArray();
 
 	}
 
 	// This function returns an array with a list of serials, that requested the server the most,
 	// limited by the int limit
-	private function getMostRequestsBySerial($limit): Array{
+	public function getMostRequestsBySerial($limit): Array{
 
-		// functionality goes here
+		$serialArray = array();
+		
+		echo "\nCounting serials...\n";
+
+		for($i = 0; $i < count($this->sepoLog); $i++){
+
+			$serial = $this->sepoLog[$i]["serial"];
+
+
+			if(array_key_exists($serial, $serialArray)){
+
+				$serialArray[$serial] += 1;
+
+			}
+
+			else{
+
+				$serialArray[$serial] = 1;
+
+			}
+
+		}
+
+		arsort($serialArray);
+
+		$i = 0;
+		$top = array();
+
+		foreach($serialArray as $key => $value){
+
+			$top[$key] = $value;
+			if(++$i == $limit) break;
+
+		}
+
+		return $top;
 
 	}
 
 	// This function returns an array with a top list of serials,
 	// that are installed on multiple devices
-	private function getMostUsedSerials($limit): Array{
+	public function getMostUsedSerials($limit): Array{
 
 		// functionality goes here
 
@@ -31,10 +66,10 @@ class SepoLogAnalyzer{
 
 	// This function returns an array with a "cpu, mem" key and the number of devices in use
 	// with that combination
-	private function getDevicesByHardware(): Array{
+	public function getDevicesByHardware(): Array{
 
 		//functionality goes here
-		
+
 	}
 
 }
