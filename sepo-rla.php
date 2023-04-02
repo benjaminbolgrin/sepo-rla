0be1d346d77de44ef833d981ab7c7e1d7e58840b
Benjamin Bolgrin <>
2023-04-02 18:38:36 +0200

    Added cli parameters to the sepo-rla program

--- sepo-rla.php
+++ sepo-rla.php
@@ -11,6 +11,9 @@ function main(){
 
 	// A file path has to be given as an argument
 	$shortOpt = 'p:';
+	$shortOpt .= 's::';
+	$shortOpt .= 'm::';
+	$shortOpt .= 'd';
 
 	$longOpt = array(
 		'help');
@@ -20,7 +23,15 @@ function main(){
 
 	// Help argument was given
 	if (isset($options['help'])){
-		echo "\nPlease provide a file path to a log file by using the -p parameter followed by a file path like this:\nsepo-rla.php -p '/path/to/your/file.log'\n";
+		echo "\n-p\t\tPlease provide a file path to a log file by using\n
+		the -p parameter followed by a file path like this:\n
+		sepo-rla.php -p '/path/to/your/file.log'\n\n
+		\n-s\t\tOptional Parameter to display the top requests by serial\n
+		Use -s10 to display the top ten entries\n\n
+		\n-m\t\tOptional parameter to display an array with top serials used on multiple devices\n
+		Use -m10 to display the top ten entries\n\n
+		\n-d\t\tOptional parameter to display information about hardware in use
+		\n\n";
 		exit();
 	}
 
@@ -33,7 +44,46 @@ function main(){
 		// Validate path
 		if(file_exists($filePath)){
 
-			// functionality goes here
+			$logArray = new SepoLog($filePath);
+			$logAnalyzer = new SepoLogAnalyzer($logArray);
+
+			// If the program has been started with the s argument, display an array with top requests by serial numbers
+			if(isset($options['s'])){
+
+				$optS = (int)$options['s'];
+
+				if(is_int($optS)){
+
+					$sLimit = $optS;
+
+					var_dump($logAnalyzer->getMostRequestsBySerial($sLimit));
+
+				}
+
+			}
+
+			// If the program has been started with the m argument, display an array with top serials used on multiple devices
+			if(isset($options['m'])){
+
+				$optM = (int)$options['m'];
+
+				if(is_int($optM)){
+
+					$mLimit = $optM;
+
+					var_dump($logAnalyzer->getMostUsedSerials($mLimit));
+
+				}
+
+			}
+
+			// If the program has been started with die d argument,
+			// display information about hardware in use
+			if(isset($options['d'])){
+
+				var_dump($logAnalyzer->getDevicesByHardware());
+
+			}
 
 		}
 
